d363cf88ab8695a7fda805f41c87fe873a2f7984
Raymond Lynch <daemitus@gmail.com>
2021-01-23 12:51:36 -0500

    Initial codebase

---
+++ WondrousTailsSolver/PerfectTails.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WondrousTailsSolver
+{
+    public sealed class PerfectTails
+    {
+        public const int TotalStickers = 16;
+        public const int StickersPerLane = 4;
+
+        private readonly Dictionary<int, long[]> PossibleBoards = new Dictionary<int, long[]>();
+        private readonly Dictionary<int, double[]> SampleProbs = new Dictionary<int, double[]>();
+
+        public PerfectTails()
+        {
+            CalculateBoards(0, 0, 0, 0, 0);
+            CalculateSamples();
+        }
+
+        public double[] Solve(bool[] cells)
+        {
+            var mask = CellsToMask(cells);
+            var counts = PossibleBoards[mask];
+
+            var divisor = (double)counts[0];
+            var probs = counts.Skip(1).Select(c => Math.Round(c / divisor, 4)).ToArray();
+
+            return probs;
+        }
+
+        public double[] GetSample(int stickersPlaced)
+        {
+            if (SampleProbs.TryGetValue(stickersPlaced, out var probs))
+                return probs;
+            return null;
+        }
+
+        private long[] CalculateBoards(int mask, int numStickers, int numRows, int numCols, int numDiags)
+        {
+            if (PossibleBoards.TryGetValue(mask, out var result))
+                return result;
+
+            if (numStickers == 9)
+            {
+                var lines = numRows + numCols + numDiags;
+                return PossibleBoards[mask] = new long[] {
+                    1,
+                    lines >= 1 ? 1 : 0,
+                    lines >= 2 ? 1 : 0,
+                    lines >= 3 ? 1 : 0
+                };
+            }
+
+            if (numStickers > 9)
+            {
+                return new long[] { 0, 0, 0, 0 };
+            }
+
+            result = PossibleBoards[mask] = new long[] { 0, 0, 0, 0 };
+
+            for (var r = 0; r < 4; r++)
+            {
+                for (var c = 0; c < 4; c++)
+                {
+                    if (MaskHasBit(mask, r, c))
+                        continue;
+
+                    var nMask = SetMaskBit(mask, r, c);
+                    var nRows = MaskHasRow(nMask, r) ? 1 : 0;
+                    var nCols = MaskHasCol(nMask, c) ? 1 : 0;
+                    var nDiag1 = MaskHasDiag1(nMask) ? 1 : 0;
+                    var nDiag2 = MaskHasDiag2(nMask) ? 1 : 0;
+                    var nResult = CalculateBoards(nMask, numStickers + 1, numRows + nRows, numCols + nCols, numDiags + nDiag1 + nDiag2);
+
+                    for (var i = 0; i < 4; i++)
+                    {
+                        result[i] += nResult[i];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void CalculateSamples()
+        {
+            Random random = new Random();
+            for (int stickersPlaced = 1; stickersPlaced <= 7; stickersPlaced++)
+            {
+                var samples = new List<double[]>();
+                for (int i = 0; i < 50; i++)
+                {
+                    var sampleState = new bool[16];
+                    foreach (var sampleIndex in Enumerable.Range(0, 16).OrderBy(v => random.Next()).Take(stickersPlaced))
+                        sampleState[sampleIndex] = true;
+                    samples.Add(Solve(sampleState));
+                }
+                var avgSolution = new double[]
+                {
+                    Math.Round(samples.Average(s => s[0]), 4),
+                    Math.Round(samples.Average(s => s[1]), 4),
+                    Math.Round(samples.Average(s => s[2]), 4),
+                };
+                SampleProbs[stickersPlaced] = avgSolution;
+            }
+        }
+
+        private int CellsToMask(bool[] cells)
+        {
+            var mask = 0;
+            for (var r = 0; r < 4; r++)
+            {
+                for (var c = 0; c < 4; c++)
+                {
+                    if (cells[r * 4 + c])
+                        mask = SetMaskBit(mask, r, c);
+                }
+            }
+            return mask;
+        }
+
+        private int GetMaskBit(int r, int c) => 1 << (4 * r + c);
+
+        private int SetMaskBit(int mask, int r, int c) => mask | GetMaskBit(r, c);
+
+        private bool MaskHasBit(int mask, int r, int c) => (mask & GetMaskBit(r, c)) == GetMaskBit(r, c);
+
+        private bool MaskHasRow(int mask, int r) => Enumerable.Range(0, 4).All(c => MaskHasBit(mask, r, c));
+
+        private bool MaskHasCol(int mask, int c) => Enumerable.Range(0, 4).All(r => MaskHasBit(mask, r, c));
+
+        private bool MaskHasDiag1(int mask) => Enumerable.Range(0, 4).All(i => MaskHasBit(mask, i, i));
+
+        private bool MaskHasDiag2(int mask) => Enumerable.Range(0, 4).All(i => MaskHasBit(mask, i, 3 - i));
+    }
+}
---
+++ WondrousTailsSolver/WondrousTailsSolverPlugin.cs
@@ -0,0 +1,143 @@
+﻿using Dalamud.Plugin;
+using ImGuiNET;
+using System;
+using System.Linq;
+using System.Numerics;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using System.Threading.Tasks;
+using System.Threading;
+using Dalamud.Hooking;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WondrousTailsSolver
+{
+    public sealed class WondrousTailsSolverPlugin : IDalamudPlugin
+    {
+        public string Name => "ezWondrousTails";
+
+        internal DalamudPluginInterface Interface;
+        internal PluginAddressResolver Address;
+
+        private Hook<AddonWeeklyBingo_Ctor_Delegate> AddonWeeklyBingo_Ctor_Hook;
+        private AtkTextNode_SetText_Delegate AtkTextNode_SetText;
+        private CancellationTokenSource LoopTokenSource;
+
+        private const int TotalStickers = PerfectTails.TotalStickers;
+        private const int TotalLanes = PerfectTails.StickersPerLane;
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            Interface = pluginInterface ?? throw new ArgumentNullException(nameof(pluginInterface), "DalamudPluginInterface cannot be null");
+
+            Address = new PluginAddressResolver();
+            Address.Setup(Interface.TargetModuleScanner);
+
+            AddonWeeklyBingo_Ctor_Hook = new Hook<AddonWeeklyBingo_Ctor_Delegate>(Address.AddonWeeklyBingo_Ctor_Address, new AddonWeeklyBingo_Ctor_Delegate(AddonWeeklyBingo_Ctor_Detour), this);
+            AtkTextNode_SetText = Marshal.GetDelegateForFunctionPointer<AtkTextNode_SetText_Delegate>(Address.AtkTextNode_SetText_Address);
+
+            AddonWeeklyBingo_Ctor_Hook.Enable();
+        }
+
+        public void Dispose()
+        {
+            LoopTokenSource?.Cancel();
+
+            AddonWeeklyBingo_Ctor_Hook?.Disable();
+            AddonWeeklyBingo_Ctor_Hook?.Dispose();
+        }
+
+        #region Hooks
+
+        private IntPtr AddonWeeklyBingo_Ctor_Detour(IntPtr a1)
+        {
+            var addonPtr = AddonWeeklyBingo_Ctor_Hook.Original(a1);
+
+            for (int i = 0; i < TotalStickers; i++)
+                GameState[i] = true;
+
+            LoopTokenSource?.Cancel();
+            LoopTokenSource = new CancellationTokenSource();
+            Task.Run(() => UpdateTextLoop(addonPtr, LoopTokenSource.Token));
+            return addonPtr;
+        }
+
+        private unsafe void UpdateTextLoop(IntPtr addonPtr, CancellationToken token)
+        {
+            if (addonPtr == IntPtr.Zero)
+                return;
+
+            var addon = (AddonWeeklyBingo*)addonPtr;
+
+            if (addon == null)
+                return;
+
+            while (!token.IsCancellationRequested && addon->AtkUnitBase.IsVisible)
+            {
+                if (addon->AtkUnitBase.ULDData.LoadedState == 3 && addon->AtkUnitBase.ULDData.NodeListCount >= 96)
+                {
+                    var textNode = (AtkTextNode*)addon->AtkUnitBase.ULDData.NodeList[96];
+                    if (textNode != null)
+                    {
+                        UpdateText(addon, textNode);
+                    }
+                }
+                Task.Delay(100).Wait();
+            }
+        }
+
+        private readonly bool[] GameState = new bool[TotalStickers];
+
+        private unsafe void UpdateText(AddonWeeklyBingo* addon, AtkTextNode* textNode)
+        {
+            bool stateChanged = false;
+            for (var i = 0; i < TotalStickers; i++)
+            {
+                var node = addon->StickerSlotList[i].StickerComponentBase;
+                if (node == null)
+                    return;
+
+                var parentNode = node->OwnerNode->AtkResNode.ParentNode;
+                var state = parentNode->IsVisible;
+                stateChanged |= GameState[i] != state;
+                GameState[i] = state;
+            }
+
+            if (!stateChanged)
+                return;
+
+            var stickersPlaced = GameState.Count(s => s);
+            if (stickersPlaced == 9)
+                return;
+
+            var currentText = Marshal.PtrToStringAnsi(new IntPtr(textNode->NodeText.StringPtr));
+
+            var sb = new StringBuilder();
+            var probs = PerfectTails.Solve(GameState);
+            sb.AppendLine(currentText);
+            sb.AppendLine($"1 Line: {probs[0] * 100:F2}%");
+            sb.AppendLine($"2 Lines: {probs[1] * 100:F2}%");
+            sb.AppendLine($"3 Lines: {probs[2] * 100:F2}%");
+
+            if (stickersPlaced > 0 && stickersPlaced <= 7)
+            {
+                var sample = PerfectTails.GetSample(stickersPlaced);
+                sb.Append($"Shuffle Average: ");
+                sb.Append($"{sample[0] * 100:F2}%   ");
+                sb.Append($"{sample[1] * 100:F2}%   ");
+                sb.Append($"{sample[2] * 100:F2}%   ");
+            }
+
+            var textNodePtr = new IntPtr(textNode);
+            var textPtr = Marshal.StringToHGlobalAnsi(sb.ToString());
+            AtkTextNode_SetText(new IntPtr(textNode), textPtr, IntPtr.Zero);
+            Marshal.FreeHGlobal(textPtr);
+        }
+
+        #endregion
+
+        private readonly PerfectTails PerfectTails = new PerfectTails();
+    }
+}
