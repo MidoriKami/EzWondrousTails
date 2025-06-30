034434e772b269a13f39dd833dd38128c920d910
Hakuna9889 <hakuna9889@gmail.com>
2025-06-30 19:32:05 +0300

    [fix] Repair of the сurrently occupied duty highlighting feature

--- WondrousTailsSolver/AddonWeeklyBingoController.cs
+++ WondrousTailsSolver/AddonWeeklyBingoController.cs
@@ -3,9 +3,12 @@ using System.Linq;
 using System.Numerics;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using Dalamud.Game.ClientState.Conditions;
+using Dalamud.Utility.Numerics;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiLib.Classes;
 using KamiToolKit.Classes;
 using KamiToolKit.Extensions;
 using KamiToolKit.Nodes;
@@ -14,15 +17,20 @@ namespace WondrousTailsSolver;
 
 public unsafe class AddonWeeklyBingoController : IDisposable {
     private TextNode? probabilityTextNode;
-    
+    private AtkNineGridNode* currentDutyNode;
+    private readonly Configuration configuration;
+
     public AddonWeeklyBingoController() {
+        configuration = Service.Interface.GetPluginConfig() as Configuration ?? new Configuration();
+
         Service.AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "WeeklyBingo", OnAddonSetup);
         Service.AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "WeeklyBingo", OnAddonFinalize);
         Service.AddonLifecycle.RegisterListener(AddonEvent.PostRefresh, "WeeklyBingo", OnAddonRefresh);
+        Service.AddonLifecycle.RegisterListener(AddonEvent.PostDraw, "WeeklyBingo", AddonDrawDetour);
     }
-    
+
     public void Dispose() {
-        Service.AddonLifecycle.UnregisterListener(OnAddonSetup, OnAddonFinalize, OnAddonRefresh);
+        Service.AddonLifecycle.UnregisterListener(OnAddonSetup, OnAddonFinalize, OnAddonRefresh, AddonDrawDetour);
 
         var addon = (AddonWeeklyBingo*) Service.GameGui.GetAddonByName("WeeklyBingo");
         if (addon is not null) {
@@ -46,7 +54,14 @@ public unsafe class AddonWeeklyBingoController : IDisposable {
     private void OnAddonFinalize(AddonEvent type, AddonArgs args) {
         DetachNode((AddonWeeklyBingo*)args.Addon);
     }
-    
+
+    private void AddonDrawDetour(AddonEvent type, AddonArgs args) {
+        if (currentDutyNode is null) return;
+
+        currentDutyNode->ToggleVisibility(true);
+        currentDutyNode->Color = configuration.CurrentDutyColor.ToByteColor();
+    }
+
     private void OnAddonRefresh(AddonEvent type, AddonArgs args) {
         foreach (var index in Enumerable.Range(0, 16)) {
             System.PerfectTails.GameState[index] = PlayerState.Instance()->IsWeeklyBingoStickerPlaced(index);
@@ -55,7 +70,18 @@ public unsafe class AddonWeeklyBingoController : IDisposable {
         if (probabilityTextNode is not null) {
             probabilityTextNode.Text = System.PerfectTails.SolveAndGetProbabilitySeString();
         }
+
+        if (Service.Condition.Any(ConditionFlag.BoundByDuty, ConditionFlag.BoundByDuty56, ConditionFlag.BoundByDuty95)) {
+            foreach (var index in Enumerable.Range(0, 16)) {
+                if (WondrousTailsTaskResolver.GetTerritoriesFromOrderId(Service.DataManager, PlayerState.Instance()->WeeklyBingoOrderData[index]).Contains(Service.ClientState.TerritoryType)) {
+                    currentDutyNode = GetBorderResourceNode((AddonWeeklyBingo*)args.Addon, index);
+                }
+            }
+        }
     }
+
+    private AtkNineGridNode* GetBorderResourceNode(AddonWeeklyBingo* addon, int dutySlot)
+    => (AtkNineGridNode*)addon->DutySlotList[dutySlot].DutyButton->AtkComponentBase.UldManager.SearchNodeById(11);
 
     private void AddTextNode(AtkTextNode* existingTextNode, AddonWeeklyBingo* addonWeeklyBingo) {
         probabilityTextNode = new TextNode {
